3c9c9ea79eadda2292f205af280d942d7fabdafe
Thomas Eastman <tjeastman@gmail.com>
2023-03-07 08:09:56 -0800

    use C++

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <avr/io.h>
+#include <stdbool.h>
+#include <util/delay.h>
+
+int main(void)
+{
+    DDRB |= _BV(7);
+
+    while (true) {
+        PORTB |= _BV(7);
+        _delay_ms(1000);
+        PORTB &= ~_BV(7);
+        _delay_ms(1000);
+    }
+
+    return 0;
+}
