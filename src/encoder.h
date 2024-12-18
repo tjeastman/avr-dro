34e814f2ef144d893cf82c1a787126507d35b46e
Thomas Eastman <tjeastman@gmail.com>
2024-12-18 10:14:43 -0800

    remove unused shift operation from Encoder class

--- src/encoder.h
+++ src/encoder.h
@@ -6,11 +6,10 @@ private:
     static signed char lookup[16];
     int count_;
     unsigned char state_;
-    unsigned char shift_;
 public:
-    Encoder(): count_{0}, state_{0}, shift_{0} {}
+    Encoder(): count_{0}, state_{0} {}
     void consume(unsigned char) volatile;
-    int count() { return count_ >> shift_; }
+    int count() { return count_; }
 };
 
 #endif
