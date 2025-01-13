bedb94d74373b124f60fff0e19385b4dbfc8eb98
Thomas Eastman <tjeastman@gmail.com>
2025-01-13 11:31:54 -0800

    initial implementation of G-code command queue

--- src/command.cpp
+++ src/command.cpp
@@ -27,3 +27,53 @@ const Serial &operator<<(const Serial &serial, const Command &command)
 
     return serial;
 }
+
+int CommandQueue::size() const
+{
+    return (head_ + 20 - tail_) % 20;
+}
+
+void CommandQueue::project(PendantAxis::Identifier identifier, int position, int rate)
+{
+    char axis = 0;
+    switch (identifier) {
+    case PendantAxis::Identifier::X:
+        axis = 'X';
+        break;
+    case PendantAxis::Identifier::Y:
+        axis = 'Y';
+        break;
+    case PendantAxis::Identifier::Z:
+        axis = 'Z';
+        break;
+    default:
+        return;
+    }
+
+    if (size() < 2) {
+        head_++;
+    } else if (commands_[head_].axis_ != axis) {
+        head_++;
+    }
+
+    if (head_ >= 20) {
+        head_ = 0;
+    }
+
+    commands_[head_].axis_ = axis;
+    commands_[head_].position_ = position;
+    commands_[head_].rate_ = rate;
+}
+
+void CommandQueue::something(const Serial &serial)
+{
+    if (size() == 0) {
+        return;
+    }
+
+    serial << commands_[tail_++];
+
+    if (tail_ >= 20) {
+        tail_ = 0;
+    }
+}
--- src/command.h
+++ src/command.h
@@ -14,6 +14,19 @@ public:
     Command(char axis): axis_{axis}, position_{0}, rate_{0} {}
     Command(char axis, int position, int rate): axis_{axis}, position_{position}, rate_{rate} {}
     friend const Serial &operator<<(const Serial &, const Command &);
+    friend class CommandQueue;
+};
+
+class CommandQueue : public PendantAxisSpace {
+private:
+    Command commands_[20];
+    int head_;
+    int tail_;
+public:
+    CommandQueue(): head_{0}, tail_{0} {}
+    int size() const;
+    void project(PendantAxis::Identifier, int, int);
+    void something(const Serial &);
 };
 
 #endif  // COMMAND_H_
